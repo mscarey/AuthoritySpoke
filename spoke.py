a8ce099a41d16410a15a50d10604cfe2448b1796
Matt Carey <mscarey@gmail.com>
2018-11-01 14:36:47 -0500

    reciprocal flag for factors

--- spoke.py
+++ spoke.py
@@ -1,21 +1,43 @@
+from typing import Dict, Union
+
 class Factor:
 
-    def __init__(self, content: str, level: str = "Fact"):
-        self.content = content
+    def __init__(self, predicate: str, truth_of_predicate: bool = True,
+        level: str = "Fact", reciprocal: bool = False):
+
+        self.predicate = predicate
+        self.truth_of_predicate = truth_of_predicate
         self.level = level
+        self.reciprocal = reciprocal
+
+        if len(self) != 2 and self.reciprocal:
+            raise ValueError(
+                f'"Reciprocal" flag is only allowed with exactly 2 entities.')
 
     def __str__(self):
-        return f'{self.level}: {self.content}'
+        return f'{self.level}: {self.predicate_with_truth()}'
 
     def __len__(self):
-        return self.content.count("{}")
+        return self.predicate.count("{}")
 
-    def content_with_entities(self, entities: list):
+    def predicate_with_truth(self):
+        truth = "It is false that " if not self.truth_of_predicate else ""
+        return f'{truth}{self.predicate}'
+
+    def predicate_with_entities(self, entities: list) -> str:
         """Creates a sentence by substituting the names of entities
         from a particular case."""
 
         if len(entities) != len(self):
             raise ValueError(
                 f'Exactly {len(self)} entities needed to complete ' +
-                f'"{self.content}", but {len(entities)} were given.')
-        return self.content.format(*entities)
+                f'"{self.predicate}", but {len(entities)} were given.')
+        return self.predicate_with_truth().format(*entities)
+
+class Holding:
+
+    def __init__(self, inputs: Dict[Factor, str], outputs: Dict[Factor, str],
+                 rule_valid: Union[bool, None] = True):
+
+        self.rule_valid = rule_valid
+        pass
--- test_spoke.py
+++ test_spoke.py
@@ -1,30 +1,45 @@
 from spoke import Factor
+from typing import Dict
 
 import pytest
 
 
-def test_string_representation_of_factor():
+@pytest.fixture
+def make_factor() -> Dict[str, Factor]:
     f1 = Factor("{} was a motel")
-    assert str(f1) == "Fact: {} was a motel"
+    f7 = Factor("the distance between {} and {} was more than 35 feet",
+                truth_of_predicate=False, reciprocal=True)
+    return {"f1": f1, "f7": f7}
 
-def test_entity_slots_as_length_of_factor():
-    f1 = Factor("{} was a motel")
-    assert len(f1) == 1
+def test_string_representation_of_factor(make_factor):
+    assert str(make_factor["f1"]) == "Fact: {} was a motel"
 
-def test_content_with_entities():
+def test_entity_slots_as_length_of_factor(make_factor):
+    assert len(make_factor["f1"]) == 1
+
+def test_predicate_with_entities(make_factor):
     """Entities are just strings for now"""
+    assert make_factor["f1"].predicate_with_entities(
+        ["rAnDoM STriNg"]) == "rAnDoM STriNg was a motel"
 
-    f1 = Factor("{} was a motel")
-    assert f1.content_with_entities(["rAnDoM STriNg"]) == "rAnDoM STriNg was a motel"
-
-def test_content_with_wrong_number_of_entities():
-    f1 = Factor("{} was a motel")
+def test_predicate_with_wrong_number_of_entities(make_factor):
     with pytest.raises(ValueError):
-        f1.content_with_entities(["Motel 6", "Wattenburg"])
+        f = Factor("{} was a motel", reciprocal=True)
 
-"""
-Test that holdings are considered equal if they have the same factors
-and the numbers they use to refer to entities are different but in an
-equivalent order.
-e.g. {"F1": "121", "F2": "233"} and {"F2": "122", "F1": "313"}
-"""
+def test_reciprocal_with_wrong_number_of_entities(make_factor):
+    with pytest.raises(ValueError):
+        make_factor["f1"].predicate_with_entities(["Motel 6", "Wattenburg"])
+
+def test_false_predicate_with_entities(make_factor):
+    assert make_factor["f7"].predicate_with_entities(
+        ["the trees", "Hideaway Lodge"]) == str("It is false that the " +
+        "distance between the trees and Hideaway Lodge was more than 35 feet")
+
+def test_holding_equality():
+    """
+    Test that holdings are considered equal if they have the same factors
+    and the numbers they use to refer to entities are different but in an
+    equivalent order.
+    e.g. {"F1": "121", "F2": "233"} and {"F2": "122", "F1": "313"}
+    """
+    pass
