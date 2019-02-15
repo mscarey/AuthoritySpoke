deb76493e5aaaa62b09c290190b602fc8aeafe00
Matt Carey <mscarey@gmail.com>
2019-02-15 02:09:59 -0600

    split test file

---
+++ tests/test_factors.py
@@ -0,0 +1,445 @@
+from copy import copy
+import datetime
+import json
+from typing import Dict
+
+from pint import UnitRegistry
+import pytest
+
+from enactments import Code, Enactment
+from entities import Entity, Human
+from rules import Procedure, Rule, ProceduralRule
+from opinions import Opinion
+from spoke import Predicate, Factor, Fact, Evidence
+from spoke import ureg, Q_
+from spoke import check_entity_consistency  # move this back into a class?
+from spoke import find_matches, evolve_match_list
+
+
+class TestFacts:
+    def test_default_entity_context_for_fact(
+        self, make_entity, make_predicate, watt_mentioned
+    ):
+        e = make_entity
+        f2 = Fact(make_predicate["p1"], case_factors=watt_mentioned)
+        assert f2.entity_context == (e["e_motel"],)
+
+    def test_entity_context_from_case_factor_indices(
+        self, make_entity, make_predicate, watt_mentioned
+    ):
+        """
+        If you pass in integers instead of Factor objects to fill the blanks
+        in the Predicate (which was the only way to do things in the first
+        version of the Fact class's __init__ method), then the integers
+        you pass in should be used as indices to select Factor objects
+        from case_factors.
+        """
+
+        e = make_entity
+
+        f2 = Fact(
+            make_predicate["p2"], entity_context=(1, 0), case_factors=watt_mentioned
+        )
+        assert f2.entity_context == (e["e_watt"], e["e_motel"])
+
+    def test_invalid_index_for_case_factors_in_init(self, make_predicate, make_entity):
+        with pytest.raises(ValueError):
+            _ = Fact(
+                make_predicate["p1"],
+                entity_context=2,
+                case_factors=make_entity["e_watt"],
+            )
+
+    def test_convert_int_entity_context_to_tuple(self, make_predicate, watt_mentioned):
+        f = Fact(make_predicate["p_irrelevant_1"], 3, case_factors=watt_mentioned)
+        assert f.entity_context == (watt_mentioned[3],)
+
+    def test_string_representation_of_factor(self, watt_factor):
+        assert str(watt_factor["f1"]) == "Fact: <Hideaway Lodge> was a motel"
+        assert (
+            str(watt_factor["f3_absent"])
+            == "Absent Fact: <Hideaway Lodge> was <Wattenburg>’s abode"
+        )
+
+    def test_string_representation_with_concrete_entities(self, watt_factor):
+        """
+        "Hideaway Lodge" is still a string representation of an Entity
+        object, but it's not in angle brackets because it can't be
+        replaced by another Entity object without changing the meaning
+        of the Fact.
+        """
+        assert str(watt_factor["f1_specific"]) == "Fact: Hideaway Lodge was a motel"
+
+    def test_new_concrete_context(self, watt_factor):
+        different = watt_factor["f2"].new_context(
+            [Human("He-Man"), Entity("Castle Grayskull")]
+        )
+        assert "<He-Man> operated" in str(different)
+
+    def test_concrete_to_abstract(self, make_entity, make_predicate):
+        motel = make_entity["e_motel_specific"]
+        d = make_entity["e_watt"]
+        fact = Fact(predicate=make_predicate["p2"], entity_context=(d, motel))
+        assert "<Wattenburg> operated and lived at Hideaway Lodge" in str(fact)
+        assert "<Wattenburg> operated and lived at <Hideaway Lodge>" in str(
+            fact.make_generic()
+        )
+
+    def test_entity_slots_as_length_of_factor(self, watt_factor):
+        assert len(watt_factor["f1"].predicate) == 1
+        assert len(watt_factor["f1"]) == 1
+
+    def test_entity_orders(self, make_entity, watt_factor):
+        assert watt_factor["f7_swap_entities_4"].entity_orders == {
+            (make_entity["e_watt"], make_entity["e_motel_specific"]),
+            (make_entity["e_motel_specific"], make_entity["e_watt"]),
+        }
+
+    def test_predicate_with_entities(self, make_entity, watt_factor):
+        assert (
+            watt_factor["f1"].predicate.content_with_entities((make_entity["e_motel"]))
+            == "<Hideaway Lodge> was a motel"
+        )
+
+    def test_factor_entity_context_does_not_match_predicate(self, make_predicate):
+        with pytest.raises(ValueError):
+            _ = Fact(make_predicate["p1"], (0, 1, 2))
+
+    def test_reciprocal_with_wrong_number_of_entities(self, make_entity, watt_factor):
+        with pytest.raises(ValueError):
+            watt_factor["f1"].predicate.content_with_entities(
+                (make_entity["e_motel"], make_entity["e_watt"])
+            )
+
+    def test_false_predicate_with_entities(self, make_entity, watt_factor):
+        assert watt_factor["f7"].predicate_in_context(
+            (make_entity["e_trees"], make_entity["e_motel"])
+        ) == str(
+            "Fact: The distance between <the stockpile of trees> "
+            + "and <Hideaway Lodge> was no more than 35 foot"
+        )
+
+    def test_entity_and_human_in_predicate(self, make_entity, watt_factor):
+        assert (
+            watt_factor["f2"].predicate.content_with_entities(
+                (make_entity["e_watt"], make_entity["e_motel"])
+            )
+            == "<Wattenburg> operated and lived at <Hideaway Lodge>"
+        )
+
+    def test_fact_label_with_entities(self, make_entity, watt_factor):
+        assert (
+            watt_factor["f2"].predicate_in_context(
+                (make_entity["e_watt"], make_entity["e_motel"])
+            )
+            == "Fact: <Wattenburg> operated and lived at <Hideaway Lodge>"
+        )
+
+    def test_standard_of_proof_must_be_listed(self, make_predicate):
+        with pytest.raises(ValueError):
+            _ = Fact(make_predicate["p2"], standard_of_proof="probably so")
+
+    def test_standard_of_proof_in_str(self, watt_factor):
+        factor = watt_factor["f2_preponderance_of_evidence"]
+        assert factor.standard_of_proof in str(factor)
+
+    # Equality
+
+    def test_equality_factor_from_same_predicate(self, watt_factor):
+        assert watt_factor["f1"] == watt_factor["f1b"]
+
+    def test_equality_factor_from_equal_predicate(self, watt_factor):
+        assert watt_factor["f1"] == watt_factor["f1c"]
+
+    def test_equality_because_factors_are_generic_entities(self, watt_factor):
+        assert watt_factor["f1"] == watt_factor["f1_different_entity"]
+
+    def test_unequal_because_a_factor_is_not_generic(self, watt_factor):
+        assert watt_factor["f9_swap_entities_4"] != watt_factor["f9"]
+
+    def test_generic_factors_equal(self, watt_factor):
+        assert watt_factor["f2_generic"] == watt_factor["f2_false_generic"]
+        assert watt_factor["f2_generic"] == watt_factor["f3_generic"]
+
+    def test_generic_and_specific_factors_unequal(self, watt_factor):
+        assert watt_factor["f2"] != watt_factor["f2_generic"]
+
+    def test_factor_reciprocal_unequal(self, watt_factor):
+        assert watt_factor["f2"] != watt_factor["f2_reciprocal"]
+
+    def test_factor_unequal_predicate_truth(self, watt_factor):
+        assert watt_factor["f7"] != watt_factor["f7_true"]
+        assert watt_factor["f7"].contradicts(watt_factor["f7_true"])
+
+    def test_copies_of_identical_factor(self, make_factor):
+        """
+        Even if the two factors have different entity markers in self.entity_context,
+        I expect them to evaluate equal because the choice of entity markers is
+        arbitrary.
+        """
+        f = make_factor
+        assert f["f_irrelevant_3"] == f["f_irrelevant_3"]
+        assert f["f_irrelevant_3"] == f["f_irrelevant_3_new_context"]
+
+    @pytest.mark.xfail
+    def test_unequal_due_to_repeating_entity(self, make_factor):
+        """I'm not convinced that a model of a Fact ever needs to include
+        multiple references to the same Entity just because the name of the
+        Entity appears more than once in the Predicate."""
+        f = make_factor
+        assert f["f_three_entities"] != f["f_repeating_entity"]
+
+    def test_standard_of_proof_inequality(self, watt_factor):
+
+        f = watt_factor
+        assert f["f2_clear_and_convincing"] != f["f2_preponderance_of_evidence"]
+        assert f["f2_clear_and_convincing"] != f["f2"]
+
+    # Implication
+
+    def test_specific_factor_implies_generic(self, watt_factor):
+        assert watt_factor["f2"] > watt_factor["f2_generic"]
+
+    def test_specific_implies_generic_form_of_another_fact(self, watt_factor):
+        assert watt_factor["f2"] > watt_factor["f3_generic"]
+
+    def test_specific_fact_does_not_imply_generic_entity(
+        self, make_entity, watt_factor
+    ):
+        assert not watt_factor["f2"] > make_entity["e_motel"]
+
+    def test_factor_does_not_imply_predicate(self, make_predicate, watt_factor):
+        with pytest.raises(TypeError):
+            assert not watt_factor["f8_meters"] > make_predicate["p8"]
+
+    def test_factor_implies_because_of_quantity(self, watt_factor):
+        assert watt_factor["f8_meters"] > watt_factor["f8"]
+        assert watt_factor["f8_higher_int"] > watt_factor["f8_float"]
+        assert watt_factor["f8_int"] < watt_factor["f8_higher_int"]
+
+    def test_factor_implies_no_truth_value(self, watt_factor):
+        assert watt_factor["f2"] > watt_factor["f2_no_truth"]
+        assert not watt_factor["f2_no_truth"] > watt_factor["f2"]
+
+    def test_factor_implies_because_of_exact_quantity(self, watt_factor):
+        assert watt_factor["f8_exact"] > watt_factor["f7"]
+        assert watt_factor["f8_exact"] >= watt_factor["f8"]
+
+    def test_absent_factor_implies_absent_factor_with_greater_quantity(
+        self, watt_factor
+    ):
+        assert watt_factor["f9_absent"] > watt_factor["f9_absent_miles"]
+
+    def test_equal_factors_not_gt(self, watt_factor):
+        f = watt_factor
+        assert f["f2"] >= f["f2_entity_order"]
+        assert f["f2"] <= f["f2_entity_order"]
+        assert not f["f2"] > f["f2_different_entity"]
+
+    def test_standard_of_proof_comparison(self, watt_factor):
+        f = watt_factor
+        assert f["f2_clear_and_convincing"] >= f["f2_preponderance_of_evidence"]
+        assert f["f2_beyond_reasonable_doubt"] >= f["f2_clear_and_convincing"]
+
+    def test_no_implication_between_factors_with_and_without_standards(
+        self, watt_factor
+    ):
+        f = watt_factor
+        assert not f["f2_clear_and_convincing"] > f["f2"]
+        assert not f["f2"] > f["f2_preponderance_of_evidence"]
+
+    # Contradiction
+
+    def test_factor_does_not_contradict_predicate(self, make_predicate, watt_factor):
+        with pytest.raises(TypeError):
+            _ = watt_factor["f7"].contradicts(make_predicate["p7_true"])
+
+    def test_factor_contradiction_absent_predicate(self, watt_factor):
+        assert watt_factor["f3"].contradicts(watt_factor["f3_absent"])
+        assert watt_factor["f3_absent"].contradicts(watt_factor["f3"])
+
+    def test_factor_no_contradiction_no_truth_value(self, watt_factor):
+        assert not watt_factor["f2"].contradicts(watt_factor["f2_no_truth"])
+        assert not watt_factor["f2_no_truth"].contradicts(watt_factor["f2_false"])
+
+    def test_absent_factor_contradicts_broader_quantity_statement(self, watt_factor):
+        assert watt_factor["f8_absent"].contradicts(watt_factor["f8_meters"])
+        assert watt_factor["f8_meters"].contradicts(watt_factor["f8_absent"])
+        assert watt_factor["f9_absent_miles"].contradicts(watt_factor["f9"])
+        assert watt_factor["f9"].contradicts(watt_factor["f9_absent_miles"])
+
+    # Consistency with Entity/Factor assignments
+
+    def test_copy_with_foreign_context(self, watt_mentioned, watt_factor):
+        w = watt_mentioned
+        assert (
+            watt_factor["f1"].copy_with_foreign_context({w[0]: w[2]})
+            == watt_factor["f1_different_entity"]
+        )
+
+    def test_check_entity_consistency_true(self, make_entity, make_factor):
+        f = make_factor
+        e = make_entity
+        assert check_entity_consistency(
+            f["f_irrelevant_3"],
+            f["f_irrelevant_3_new_context"],
+            {e["e_dan"]: e["e_craig"]},
+        )
+        assert check_entity_consistency(
+            f["f_irrelevant_3"],
+            f["f_irrelevant_3_new_context"],
+            {
+                e["e_alice"]: e["e_bob"],
+                e["e_bob"]: e["e_alice"],
+                e["e_craig"]: e["e_dan"],
+                e["e_dan"]: e["e_craig"],
+                e["e_circus"]: e["e_circus"],
+            },
+        )
+
+    def test_check_entity_consistency_false(self, make_entity, make_factor):
+        assert not check_entity_consistency(
+            make_factor["f_irrelevant_3"],
+            make_factor["f_irrelevant_3_new_context"],
+            {make_entity["e_circus"]: make_entity["e_alice"]},
+        )
+
+    def test_entity_consistency_identity_not_equality(self, make_entity, make_factor):
+        assert not check_entity_consistency(
+            make_factor["f_irrelevant_3"],
+            make_factor["f_irrelevant_3_new_context"],
+            {make_entity["e_dan"]: make_entity["e_dan"]},
+        )
+
+    def test_check_entity_consistency_type_error(self, make_factor, make_holding):
+        m = make_factor
+        with pytest.raises(TypeError):
+            check_entity_consistency(
+                m["f_irrelevant_3"], make_holding["h2"], (None, None, None, None, 0)
+            )
+
+    def test_consistent_entity_combinations(self, watt_factor, watt_mentioned):
+        """
+        Finds that for factor f["f7"], it would be consistent with the
+        other group of factors for f["f7"]'s two slots to be assigned
+        (0, 1) or (1, 0).
+        """
+        w = watt_mentioned
+        f = watt_factor
+        assert f["f7"].consistent_entity_combinations(
+            factors_from_other_procedure=[
+                f["f4"],
+                f["f5"],
+                f["f6"],
+                f["f7"],
+                f["f8"],
+                f["f9"],
+            ],
+            matches={w[2]: None, w[0]: None},
+        ) == [{w[2]: w[2], w[0]: w[0]}, {w[2]: w[0], w[0]: w[2]}]
+
+
+class TestEvidence:
+    def test_make_evidence_object(self, watt_factor):
+        e = Evidence(form="testimony", to_effect=watt_factor["f2"])
+        assert not e.absent
+
+    def test_default_len_based_on_unique_entity_slots(
+        self, make_predicate, make_factor
+    ):
+        """same as e["e_no_shooting"]"""
+
+        e = Evidence(
+            form="testimony",
+            to_effect=Fact(Predicate("{} commited a crime", truth=False)),
+            statement=Fact(Predicate("{} did not shoot {}")),
+            stated_by=0,
+        )
+        assert len(e) == 2
+
+    def test_get_entity_orders(self, make_evidence):
+        # TODO: check this after making Evidence.__str__ method
+        assert make_evidence["e_no_shooting"].entity_orders == {(0, 1, 0, 0)}
+        assert make_evidence["e_reciprocal"].entity_orders == {
+            (0, 1, 0, 2),
+            (1, 0, 0, 2),
+        }
+
+    def test_get_entity_orders_no_statement(self, make_factor):
+        e = Evidence(
+            form="testimony", to_effect=make_factor["f_no_crime"], derived_from=1
+        )
+        assert e.entity_orders == {(0, 1)}
+
+    def test_evidence_str(self, make_evidence):
+        assert str(make_evidence["e_reciprocal"]).startswith(
+            "Testimony, with a statement by <2>"
+        )
+
+    def test_equality_with_entity_order(self, make_predicate, make_evidence):
+        e = make_evidence
+        assert e["e_no_shooting"] == e["e_no_shooting_entity_order"]
+
+    def test_equality_with_no_statement(self, make_evidence):
+        assert make_evidence["e_crime"] == make_evidence["e_crime"]
+
+    def test_unequal_due_to_entity_order(self, make_evidence):
+        e = make_evidence
+        assert e["e_no_shooting"] != e["e_no_shooting_different_witness"]
+
+    def test_unequal_different_attributes(self, make_evidence):
+        assert (
+            make_evidence["e_no_shooting_no_effect_entity_order"]
+            != make_evidence["e_no_shooting_derived_from_entity_order"]
+        )
+
+    def test_implication_missing_witness(self, make_evidence):
+        e = make_evidence
+        assert e["e_no_shooting"] >= e["e_no_shooting_witness_unknown"]
+
+    def test_implication_missing_effect(self, make_evidence):
+        e = make_evidence
+        assert e["e_no_shooting"] >= e["e_no_shooting_no_effect_entity_order"]
+
+    def test_no_implication_of_fact(self, make_predicate, make_evidence):
+        assert not make_evidence["e_no_shooting"] > Fact(
+            make_predicate["p_no_shooting"]
+        )
+        assert (
+            not Fact(make_predicate["p_no_shooting"]) > make_evidence["e_no_shooting"]
+        )
+
+    def test_no_contradiction_of_fact(self, make_predicate, make_evidence):
+        assert not make_evidence["e_no_shooting"].contradicts(
+            Fact(make_predicate["p_no_shooting"])
+        )
+
+    def test_no_contradiction_from_supporting_contradictory_facts(self, make_evidence):
+        assert not make_evidence["e_no_shooting"].contradicts(
+            make_evidence["e_shooting"]
+        )
+
+    def test_contradiction_of_absent_version_of_self(self, make_evidence):
+        e = make_evidence
+        assert e["e_no_shooting"].contradicts(e["e_no_shooting_absent"])
+
+    def test_contradict_absent_version_of_implied_factor(self, make_evidence):
+        e = make_evidence
+        assert e["e_no_shooting_witness_unknown_absent"].contradicts(e["e_no_shooting"])
+        assert e["e_no_shooting"].contradicts(e["e_no_shooting_witness_unknown_absent"])
+
+    def test_no_contradiction_absent_same_witness(self, make_evidence):
+        e = make_evidence
+        assert not e["e_no_shooting_absent"].contradicts(
+            e["e_no_shooting_witness_unknown"]
+        )
+        assert not e["e_no_shooting_witness_unknown"].contradicts(
+            e["e_no_shooting_absent"]
+        )
+        assert not e["e_no_shooting_absent"].contradicts(
+            e["e_no_shooting_different_witness"]
+        )
+
+    def test_no_contradiction_of_implied_factor(self, make_evidence):
+        e = make_evidence
+        assert not e["e_no_shooting"].contradicts(e["e_no_shooting_witness_unknown"])
