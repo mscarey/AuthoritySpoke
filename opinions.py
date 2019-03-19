69518eeeaa6c9015407b6f9a3039cc3f8b23b5b9
Matt Carey <mscarey@gmail.com>
2019-03-19 12:51:56 -0500

    start on Allegation class

--- opinions.py
+++ opinions.py
@@ -7,6 +7,7 @@ import pathlib
 
 from dataclasses import dataclass
 
+from allegations import Allegation
 from evidence import Exhibit, Evidence
 from enactments import Enactment
 from facts import Fact
