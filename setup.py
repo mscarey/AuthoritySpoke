10903422c9b75152ae5fcf76bfbb744e94187a04
Matt Carey <matt@authorityspoke.com>
2019-03-27 00:27:21 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AuthoritySpoke",
+    version="0.0.1",
+    author="Matt Carey",
+    author_email="matt@authorityspoke.com",
+    description="Reading the law for the last time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mscarey/AuthoritySpoke",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Legal Industry",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Sociology :: History",
+    ],
+)
