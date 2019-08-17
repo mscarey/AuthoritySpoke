68d559f908ba3e3591c4454f1111a328f40928e0
Matt Carey <matt@authorityspoke.com>
2019-08-17 01:43:16 -0500

    add context register tests

--- tests/test_analogies.py
+++ tests/test_analogies.py
@@ -2,8 +2,10 @@ import operator
 
 import pytest
 
-from authorityspoke.factors import Analogy, Factor, ContextRegister, means
+from authorityspoke.factors import Analogy, Entity, Factor
+from authorityspoke.factors import ContextRegister, means
 from authorityspoke.facts import Fact
+from authorityspoke.io.readers import read_fact
 
 
 class TestAnalogies:
@@ -18,3 +20,25 @@ class TestAnalogies:
             comparison=operator.ge,
         )
         assert test_analogy.comparison == operator.ge
+
+
+class TestContext:
+    def test_impossible_register(self):
+        fact_al = read_fact("{Al} sold {the bull} to {Betty}.")
+        fact_alice = read_fact("{Alice} sold {the cow} to {Bob}.")
+        context = ContextRegister({Entity("Al"): Entity("Bob")})
+        analogy = Analogy(
+            need_matches=[fact_al], available=[fact_alice], comparison=means
+        )
+        answers = analogy.ordered_comparison(matches=context)
+        assert not any(answers)
+
+    def test_possible_register(self):
+        fact_al = read_fact("{Al} sold {the bull} to {Betty}.")
+        fact_alice = read_fact("{Alice} sold {the cow} to {Bob}.")
+        context = ContextRegister({Entity("Al"): Entity("Alice")})
+        analogy = Analogy(
+            need_matches=[fact_al], available=[fact_alice], comparison=means
+        )
+        answer = next(analogy.ordered_comparison(matches=context))
+        assert Entity("the bull") in answer.keys()
