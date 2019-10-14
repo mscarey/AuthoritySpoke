cca67f77f3f9a1cfce99936510bf8ef4bbdf3218
Matt Carey <matt@authorityspoke.com>
2019-10-14 03:37:26 -0500

    add selector IO tests to tests/io/

---
+++ tests/io/test_selector.py
@@ -0,0 +1,38 @@
+from authorityspoke.io import dump, readers, schemas
+
+
+class TestLoadSelector:
+    def test_get_schema_for_selector(self):
+        data = {"text": "process, system,|method of operation|, concept, principle"}
+        selector = readers.read_selector(data)
+        schema = schemas.get_schema_for_item(selector)
+        assert isinstance(schema, schemas.SelectorSchema)
+
+
+class TestDumpSelector:
+    def test_dump_selector(self):
+        """
+        Uses text from "path": "/us/usc/t17/s102/b", but
+        no longer includes a reference to the path.
+        """
+
+        data = {"text": "process, system,|method of operation|, concept, principle"}
+        selector = readers.read_selector(data)
+        selector_dict = dump.to_dict(selector)
+        assert isinstance(selector_dict, dict)
+        assert selector_dict["prefix"].startswith("process, system")
+
+    def test_string_dump_selector(self):
+        data = {"text": "process, system,|method of operation|, concept, principle"}
+        selector = readers.read_selector(data)
+        selector_str = dump.to_json(selector)
+        assert isinstance(selector_str, str)
+        assert '"prefix": "process, system' in selector_str
+
+    def test_round_trip_dict(self):
+        data = {"exact": "method of operation"}
+        selector = readers.read_selector(data)
+        selector_dict = dump.to_dict(selector)
+        new = readers.read_selector(selector_dict)
+        assert new.prefix is None
+        assert new.exact == "method of operation"
--- tests/test_selectors.py
+++ tests/test_selectors.py
@@ -113,38 +113,3 @@ class TestSelectors:
 
         holdings = loaders.load_holdings("holding_feist.json", regime=make_regime)
         assert len(holdings[7].selectors) == 2
-
-
-class TestDump:
-    def test_get_schema_for_selector(self):
-        data = {"text": "process, system,|method of operation|, concept, principle"}
-        selector = references.read_selector(data)
-        schema = get_schema_for_item(selector)
-        assert isinstance(schema, SelectorSchema)
-
-    def test_dump_selector(self):
-        """
-        Uses text from "path": "/us/usc/t17/s102/b", but
-        no longer includes a reference to the path.
-        """
-
-        data = {"text": "process, system,|method of operation|, concept, principle"}
-        selector = references.read_selector(data)
-        selector_dict = to_dict(selector)
-        assert isinstance(selector_dict, dict)
-        assert selector_dict["prefix"].startswith("process, system")
-
-    def test_string_dump_selector(self):
-        data = {"text": "process, system,|method of operation|, concept, principle"}
-        selector = references.read_selector(data)
-        selector_str = to_json(selector)
-        assert isinstance(selector_str, str)
-        assert '"prefix": "process, system' in selector_str
-
-    def test_round_trip_dict(self):
-        data = {"exact": "method of operation"}
-        selector = references.read_selector(data)
-        selector_dict = to_dict(selector)
-        new = references.read_selector(selector_dict)
-        assert new.prefix is None
-        assert new.exact == "method of operation"
