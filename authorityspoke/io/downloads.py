bc6030e5cce48ec17a7fe44ba1cc9e8f9f58d2a3
Matt Carey <matt@authorityspoke.com>
2021-07-25 18:42:25 -0500

    LegisClient can be imported from downloads module

--- authorityspoke/io/downloads.py
+++ authorityspoke/io/downloads.py
@@ -5,5 +5,6 @@ from __future__ import annotations
 from typing import Any, Dict, List, Optional, Union
 
 from justopinion.download import CAPClient, CaseAccessProjectAPIError
+from legislice.download import Client as LegisClient
 
 from authorityspoke.decisions import CAPCitation, Decision
