274e76750135c9eaeab58f620e81d5456501ae12
Matt Carey <matt@authorityspoke.com>
2021-10-04 13:17:18 -0500

    Comparison init will split content to get sign and expression
    Re-adding a feature that I don't remember deleting

--- tests/io/test_predicate.py
+++ tests/io/test_predicate.py
@@ -15,88 +15,72 @@ class TestPredicateLoad:
     """
 
     def test_load_just_content(self):
-        schema = schemas_yaml.PredicateSchema()
-        p4 = schema.load({"content": "$person was on the premises of $place"})
+        data = {"content": "$person was on the premises of $place"}
+        p4 = Predicate(**data)
         assert p4.truth is True
 
     def test_load_comparison_not_ending_with_was(self):
-        schema = schemas_yaml.PredicateSchema()
+        data = {
+            "content": "the distance between $place1 and $place2 was 35 feet",
+            "truth": True,
+            "sign": "!=",
+            "expression": "35 feet",
+        }
         with pytest.raises(ValueError):
-            schema.load(
-                {
-                    "content": "the distance between $place1 and $place2 was 35 feet",
-                    "truth": True,
-                    "sign": "!=",
-                    "expression": "35 feet",
-                }
-            )
+            Comparison(**data)
 
     def test_load_comparison(self):
-        schema = schemas_json.PredicateSchema()
-        p7 = schema.load(
-            {
-                "content": "the distance between $place1 and $place2 was",
-                "truth": True,
-                "sign": "!=",
-                "expression": "35 feet",
-            }
-        )
+        data = {
+            "content": "the distance between $place1 and $place2 was",
+            "truth": True,
+            "sign": "!=",
+            "expression": "35 feet",
+        }
+        p7 = Comparison(**data)
         assert p7.sign == "!="
 
     def test_load_and_find_quantity(self):
-        schema = schemas_yaml.PredicateSchema()
-        p7 = schema.load(
-            data={
-                "content": "the distance between $place1 and $place2 was > 35 feet",
-                "truth": True,
-            }
-        )
+        data = {
+            "content": "the distance between $place1 and $place2 was > 35 feet",
+            "truth": True,
+        }
+        p7 = Comparison(**data)
         assert p7.sign == ">"
 
     def test_load_and_normalize_quantity(self):
-        schema = schemas_yaml.PredicateSchema()
-        p7 = schema.load(
-            data={
-                "content": "the distance between $place1 and $place2 was != 35 feet",
-                "truth": True,
-            }
-        )
+        data = {
+            "content": "the distance between $place1 and $place2 was != 35 feet",
+            "truth": True,
+        }
+        p7 = Comparison(**data)
         assert p7.sign == "!="
 
     def test_load_and_normalize_comparison(self):
-        schema = schemas_json.PredicateSchema()
-        p7 = schema.load(
-            data={
-                "content": "the distance between $place1 and $place2 was",
-                "truth": True,
-                "sign": "!=",
-                "expression": "35 feet",
-            }
-        )
-        assert p7.sign == "!="
-
-    def test_read_quantity(self):
-        quantity = Comparison.read_quantity("35 feet")
-        assert str(quantity.units) == "foot"
+        data = {
+            "content": "the distance between $place1 and $place2 was",
+            "truth": True,
+            "sign": "!=",
+            "expression": "35 feet",
+        }
+        statement = Comparison(**data)
+        assert statement.sign == "!="
 
     def test_make_comparison_when_absent(self):
-        schema = schemas_yaml.PredicateSchema()
-        statement = schema.load(
-            {"content": "$person's favorite number was", "expression": 42}
+        statement = Comparison(
+            **{"content": "$person's favorite number was", "expression": 42}
         )
         assert statement.sign == "=="
         assert "$person's favorite number was exactly equal to 42" in str(statement)
         assert len(statement) == 1
 
     def test_load_predicate_with_date_expression(self):
-        schema = schemas_yaml.PredicateSchema()
         data = {
             "content": "the date when $work was created was",
             "expression": "1978-01-01",
             "sign": ">=",
             "truth": True,
         }
-        statement = schema.load(data)
+        statement = Comparison(**data)
         assert statement.quantity == date(1978, 1, 1)
 
 
@@ -109,14 +93,14 @@ class TestPredicateDump:
             expression=Q_("35 feet"),
         )
         dumped = predicate.dict()
-        assert dumped["expression"] == "35 foot"
+        assert dumped["quantity_range"]["quantity"] == "35 foot"
 
     def test_round_trip(self):
-        statement = Predicate.load(
-            {"content": "{}'s favorite number was", "expression": 42}
+        statement = Comparison(
+            **{"content": "{}'s favorite number was", "expression": 42}
         )
         dumped = statement.dict()
-        new_statement = Predicate(**dumped)
+        new_statement = Comparison(**dumped)
         assert "{}'s favorite number was exactly equal to 42" in str(new_statement)
 
     def test_dump_predicate_with_date_expression(self):
@@ -126,4 +110,4 @@ class TestPredicateDump:
             expression=date(1978, 1, 1),
         )
         dumped = copyright_date_range.dict()
-        assert dumped["expression"] == "1978-01-01"
+        assert dumped["quantity_range"]["quantity"] == date(1978, 1, 1)
